f81cbe472c7f5c2257082947d202e63e64ec7d64
J-Babi <88941451+J-Babi@users.noreply.github.com>
2022-08-26 11:28:13 -0400

    Update User.java

--- src/main/java/com/mt/springmongo/User.java
+++ src/main/java/com/mt/springmongo/User.java
@@ -90,9 +90,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-/**
+
  * Controller which handles reqest for saving {@link User}s.
- *
+/**
  * @author 
  */
 @Controller
